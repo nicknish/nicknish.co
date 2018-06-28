35d168d8779cc398e7ce533d48626137acfedfec
nicknish <nickjnish@gmail.com>
2018-06-27 19:27:26 -0700

    Add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,31 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+const HeaderLink = ({ to, children }) => (
+  <Link to={to} className="header-navlink" activeClassName="active">
+    {children}
+  </Link>
+)
+
+const Header = () => (
+  <header className="header container">
+    <Link to="/" className="header-logo">
+      NN
+    </Link>
+    <nav className="header-nav">
+      <ul className="header-navlinks">
+        <li>
+          <HeaderLink to="/work">Work</HeaderLink>
+        </li>
+        <li>
+          <HeaderLink to="/projects">Projects</HeaderLink>
+        </li>
+        <li>
+          <HeaderLink to="/blog">Blog</HeaderLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 export default Header
