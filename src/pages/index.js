800194bb3a67d2336751e4eb5b9243b23259e987
nicknish <nickjnish@gmail.com>
2018-06-27 19:31:48 -0700

    Port landing page, update blog link to point to /blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,79 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import LandingImage from '../images/landing_image_cropped.jpg'
+
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="landing">
+    <section className="container hero">
+      <h1 className="landing-title">
+        Hi, I
+        {"'"}
+        m Nick! I am a Frontend / Full Stack Developer living in Los Angeles,
+        CA.
+      </h1>
+    </section>
+
+    <section className="container--lg">
+      <img src={LandingImage} alt="Me and my girlfriend!" />
+    </section>
+
+    <section className="landing-section container">
+      <h3>
+        About{' '}
+        <span role="img" aria-label="Wave">
+          👋
+        </span>
+      </h3>
+      <p>
+        I
+        {"'"}
+        m a passionate creative with a deep love for creating products and the
+        craft of telling important stories. Because I love frontend engineering,
+        I consider myself a frontend developer first, but I
+        {"'"}
+        m a full-stack developer with JavaScript, React/Redux, Ruby, and Rails
+        experience.
+      </p>
+      <p>
+        I get stoked over conversations of product, design, and brand building.
+      </p>
+      <p>
+        I meditate, learn everyday without fail, and strive to carry joy in
+        everything I do.
+      </p>
+      <p>
+        I do some <Link to="/blog">writing</Link>
+        . I
+        {"'"}
+        m trying to build side projects daily. First and foremost, I want to
+        live a full life surrounded by people I care for.
+      </p>
+    </section>
+
+    <section className="landing-section container">
+      <h3>Experience</h3>
+      <p>
+        I
+        {"'"}
+        ve worked for Andreessen-Horowitz and YCombinator companies alike. The
+        through-line is working with other talented, passionate, kind folk.
+      </p>
+      <nav className="landing-experienceLinks">
+        <Link
+          to="/work"
+          className="landing-experienceLink btn btn-primary btn-sm"
+        >
+          See Work
+        </Link>
+        <Link
+          to="/projects"
+          className="landing-experienceLink btn btn-primary btn-sm"
+        >
+          See Projects
+        </Link>
+      </nav>
+    </section>
   </div>
 )
 
