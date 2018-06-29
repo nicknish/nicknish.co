f7dd190d2152d8a9160b095c6ef1ac132b432765
nicknish <nickjnish@gmail.com>
2018-06-29 12:49:45 -0700

    Apply app layout class names so site stretches whole height of viewport

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -6,8 +6,8 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../scss/index.scss';
 
-const Layout = ({ children, data }) => (
-  <div>
+const Layout = ({ children, data, location }) => (
+  <div className="site-container">
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
@@ -19,16 +19,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header blogUrl={data.site.siteMetadata.blog_url} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    <main className="site-content">{children()}</main>
     <Footer
       socialMediaUrls={{
         github: data.site.siteMetadata.github_url,
