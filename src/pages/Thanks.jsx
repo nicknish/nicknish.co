0555923ae0f7463a11d84a825d970f1db58be709
nicknish <nickjnish@gmail.com>
2018-06-29 18:37:55 -0700

    Add index-title class to H1s

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -3,7 +3,7 @@ import Link from 'gatsby-link';
 
 export const ContactFormSuccess = () => (
   <div className="container">
-    <h1>Thank you!</h1>
+    <h1 className="index-title">Thank you!</h1>
     <p>I will try to respond to your message within 24 hours.</p>
     <Link to="/" className="btn btn-sm btn-primary">
       Go Home
