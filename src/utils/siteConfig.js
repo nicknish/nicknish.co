92449381c97de03ed831e63afcb5e9a16887e9d5
nicknish <nickjnish@gmail.com>
2018-07-06 11:01:09 -0700

    Extract site configuration to separate module, and require it in gatsby-config

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: 'https://nicknish.co',
+  title: `Nick Nish - Developer & Startup Boy`,
+  site_description: `Nick Nish's Portfolio and Blog. Learn about life, love, and business as a millennial.`,
+  site_keywords: `developer, business, millennial`,
+  blog_url: 'https://nicknish.blog',
+  github_url: 'https://github.com/nicknish',
+  linkedin_url: 'https://linkedin.com/in/nicknish',
+  twitter_url: 'https://linkedin.com/in/nicknish'
+};
