73b49216e319c649d0acffd7ea56fb70dda26216
nicknish <nickjnish@gmail.com>
2018-07-06 11:20:45 -0700

    Add gatsby-plugin-offline to allow site to be used offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`
   ]
 };
