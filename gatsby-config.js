b227580b0b0dc1582d13f00ee9d7e0339f5ef846
nicknish <nickjnish@gmail.com>
2018-07-06 11:01:09 -0700

    Extract site configuration to separate module, and require it in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,18 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`
 });
 
+const config = require('./src/utils/siteConfig');
+
 module.exports = {
-  // Add site metadata which can be grabbed from GraphQL
   siteMetadata: {
-    siteUrl: 'https://nicknish.co',
-    title: `Nick Nish - Developer & Startup Boy`,
-    site_description: `Nick Nish's Portfolio and Blog. Learn about life, love, and business as a millennial.`,
-    site_keywords: `developer, business, millennial`,
-    blog_url: 'https://nicknish.blog',
-    github_url: 'https://github.com/nicknish',
-    linkedin_url: 'https://linkedin.com/in/nicknish',
-    twitter_url: 'https://linkedin.com/in/nicknish'
+    siteUrl: config.siteUrl,
+    title: config.title,
+    site_description: config.site_description,
+    site_keywords: config.site_keywords,
+    blog_url: config.blog_url,
+    github_url: config.github_url,
+    linkedin_url: config.linkedin_url,
+    twitter_url: config.twitter_url
   },
   plugins: [
     {
