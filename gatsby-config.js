d0229ea5954b9da30e329ec3f10a1f3a5846a450
nicknish <nickjnish@gmail.com>
2018-07-16 19:38:54 -0700

    Add comment to ensure gatsby-plugin-netlify is last

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`
+    `gatsby-plugin-netlify` // MUST BE LAST
   ]
 };
