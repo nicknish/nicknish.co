12c19b8bf5c285159ea2951a3d46c6e3a950dff4
nicknish <nickjnish@gmail.com>
2019-01-30 21:22:33 -0800

    Move StaticQuery to Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import { FaGithubAlt, FaLinkedin, FaTwitter } from 'react-icons/lib/fa';
 
 const iconChooser = key => {
@@ -14,29 +15,61 @@ const iconChooser = key => {
   }
 };
 
-export const Footer = ({ socialMediaUrls }) => (
-  <footer className="footer">
-    <nav className="footer-social">
-      <ul className="footer-socialList">
-        {Object.keys(socialMediaUrls).map(key => (
-          <li className="footer-socialListItem" key={key}>
-            <a
-              href={socialMediaUrls[key]}
-              className="footer-socialListItemLink"
-            >
-              {iconChooser(key)}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+const query = graphql`
+  query FooterQuery {
+    site {
+      siteMetadata {
+        title
+        site_description
+        site_keywords
+        github_url
+        linkedin_url
+        twitter_url
+      }
+    }
+  }
+`;
 
-    <div className="mb3 tc">
-      <span className="f6 black-30">
-        &copy; Nick Nish {new Date().getFullYear()}
-      </span>
-    </div>
-  </footer>
+export const Footer = () => (
+  <StaticQuery
+    query={query}
+    render={({
+      site: {
+        siteMetadata: { github_url, linkedin_url, twitter_url }
+      }
+    }) => {
+      const socialMediaUrls = {
+        github: github_url,
+        linkedin: linkedin_url,
+        twitter: twitter_url
+      };
+
+      return (
+        <footer className="footer">
+          <nav className="footer-social">
+            <ul className="footer-socialList">
+              {Object.keys(socialMediaUrls).map(key => (
+                <li className="footer-socialListItem" key={key}>
+                  <a
+                    href={socialMediaUrls[key]}
+                    className="footer-socialListItemLink"
+                  >
+                    {iconChooser(key)}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          <div className="mb3 tc">
+            <span className="f6 black-30">
+              &copy; Nick Nish {new Date().getFullYear()}
+            </span>
+          </div>
+        </footer>
+      );
+    }}
+  />
 );
 
 export default Footer;
