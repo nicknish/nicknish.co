f087a6ce7e7a397b529003804cb34718549158c4
Nicholas Nishiguchi <nicholas.nishiguchi@creditkarma.com>
2019-10-15 20:56:34 -0700

    Add fun developer message in dev tool console

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,21 @@
 require('prismjs/themes/prism-tomorrow.css');
 require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+
+const consoleGreetingFirstLine = `Hi there! 👋`;
+const consoleGreeting = `
+Thanks for checking out my website! If you're seeing this,
+you are awesome! I think there's a good chance we'd
+could be friends so hit me up with a message and say hello!
+
+🐦Tweet me @nickjnish saying you found my secret message
+
+`;
+
+const greetInConsoleMessage = () => {
+  if (process.env.NODE_ENV !== 'development' && typeof console === 'object') {
+    console.log(`%c${consoleGreetingFirstLine}`, 'font-size: 16px');
+    console.log(consoleGreeting);
+  }
+};
+
+greetInConsoleMessage();
