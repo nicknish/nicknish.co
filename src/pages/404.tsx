edf9b4bce8df732f5c8f4cfbe11a2f7b684ea691
Nicholas Nishiguchi <nicholas.nishiguchi@creditkarma.com>
2019-10-26 03:45:38 -0700

    Separate 404 component from page route component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/Layout/Layout';
 import { SEOTypes } from '../components/Layout/SEO';
-import Page from '../components/Layout/Page';
-import Button, { ButtonThemes, ButtonSizes } from '../components/common/Button';
+import NotFound from '../components/NotFound';
 
-import styles from '../css/404.module.css';
-
-const NotFoundPage = () => (
+const NotFoundPage: React.FC = () => (
   <Layout
     type={SEOTypes.page}
     content={{ title: 'Not Found' }}
     path="/not-found"
   >
-    <Page className={styles.page}>
-      <h1 className={styles.pageTitle}>Sorry, there{"'"}s nothing here!</h1>
-      <p>
-        That
-        {"'"}s weird. You found a dead-end. Click below to go back to the good
-        stuff.
-      </p>
-      <Button
-        to="/"
-        component={Link}
-        theme={ButtonThemes.primary}
-        size={ButtonSizes.small}
-      >
-        Go Home
-      </Button>
-    </Page>
+    <NotFound />
   </Layout>
 );
 
