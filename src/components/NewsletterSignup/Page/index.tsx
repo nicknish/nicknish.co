d5a5ac0eca9a909de8272360b86be8eb40163f87
Nicholas Nishiguchi <nicholas.nishiguchi@creditkarma.com>
2019-10-28 21:36:40 -0700

    Add newsletter page

---
+++ src/components/NewsletterSignup/Page/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Page from '../../Layout/Page';
+import NewsletterSignupForm from '../Form';
+
+import { TITLE, SUBTITLE } from '../../../constants/copy/newsletter';
+import styles from './NewsletterSignupPage.module.css';
+
+const NewsletterSignupPage = () => {
+  return (
+    <Page className={styles.newsletter}>
+      <h1 className={styles.title}>{TITLE}</h1>
+      <p>{SUBTITLE}</p>
+      <NewsletterSignupForm />
+    </Page>
+  );
+};
+
+export default NewsletterSignupPage;
