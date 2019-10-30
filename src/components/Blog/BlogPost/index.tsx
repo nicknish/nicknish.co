002975b0fd3d0cdc33b62ecfa96a4bedf12d2391
Nick Nish <nickjnish@gmail.com>
2019-10-30 01:48:03 -0700

    Add medium zoom to blog posts

--- src/components/Blog/BlogPost/index.tsx
+++ src/components/Blog/BlogPost/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, RefObject } from 'react';
 import shortid from 'shortid';
+import mediumZoom from 'medium-zoom';
 
 import Page from '../../Layout/Page';
 import Comments from '../Comments';
 import styles from './BlogPost.module.css';
 import BlogPostNewsletterSignup from '../BlogPostNewsletterSignup';
+import { prefersDarkMode } from '../../../utils/helpers';
 
 const addKeys = (arr = [], propName: string) =>
   arr.map(item => ({ key: shortid.generate(), [propName]: item }));
@@ -16,7 +18,32 @@ interface IBlogPostProps {
   tags: any;
 }
 
+const useMediumZoom = (postBodyRef: RefObject<HTMLDivElement>) => {
+  let zoom;
+
+  useLayoutEffect(() => {
+    if (postBodyRef) {
+      const $postBody = postBodyRef.current as HTMLDivElement;
+      const $images = $postBody ? $postBody.querySelectorAll('img') : [];
+
+      if ($images.length) {
+        zoom = mediumZoom($images, {
+          background: prefersDarkMode()
+            ? 'rgba(0, 0, 0, .9)'
+            : 'rgba(255,255,255, .83)',
+          scrollOffset: 10
+        });
+      }
+    }
+
+    () => zoom && zoom.detach();
+  }, [postBodyRef]);
+};
+
 const BlogPost: React.FC<IBlogPostProps> = ({ title, date, body, tags }) => {
+  const postBodyRef = useRef();
+  useMediumZoom(postBodyRef);
+
   const tagsWithKeys = tags && tags.length ? addKeys(tags, 'tag') : [];
   const tagElems = tagsWithKeys.map(tag => (
     <span className={styles.blogPostTag} key={tag.key}>
@@ -32,6 +59,7 @@ const BlogPost: React.FC<IBlogPostProps> = ({ title, date, body, tags }) => {
       </header>
 
       <div
+        ref={postBodyRef}
         className={styles.postBody}
         dangerouslySetInnerHTML={{
           __html: body.childMarkdownRemark.html
