4760ac1c3e7e0dc4a836dfd7e26555a7d580346c
Nick Nish <nicholas.nishiguchi@creditkarma.com>
2021-04-13 22:13:26 -0700

    Attempt to fix image snapshot tests in CI

--- cypress/integration/happyPath.js
+++ cypress/integration/happyPath.js
@@ -1,45 +1,49 @@
+import { runTestsWithImageSnapshots } from '../support/plugin-snapshots';
+
 describe('happy path', () => {
-  describe('blog', () => {
-    beforeEach(() => {
-      cy.visit('/');
+  runTestsWithImageSnapshots(() => {
+    describe('blog', () => {
+      beforeEach(() => {
+        cy.visit('/');
+      });
+
+      it('visits blog', () => {
+        cy.matchImageSnapshot({ name: 'blog' });
+
+        cy.get('[data-testid="BlogSeriesLink"]').first().click();
+        cy.get('[data-testid="BlogSeriesPageLink"]').first().click();
+      });
     });
 
-    it('visits blog', () => {
-      cy.matchImageSnapshot({ name: 'blog' });
+    describe('work', () => {
+      beforeEach(() => {
+        cy.visit('/work');
+      });
 
-      cy.get('[data-testid="BlogSeriesLink"]').first().click();
-      cy.get('[data-testid="BlogSeriesPageLink"]').first().click();
-    });
-  });
+      it('visits work', () => {
+        cy.matchImageSnapshot({ name: 'work' });
 
-  describe('work', () => {
-    beforeEach(() => {
-      cy.visit('/work');
-    });
+        cy.get('[data-testid="WorkSection--about"]').contains('About Me');
 
-    it('visits work', () => {
-      cy.matchImageSnapshot({ name: 'work' });
+        cy.get('[data-testid="WorkSection--career"]').contains('Career');
+        cy.get('[data-testid="WorkItem--/work/credit-karma"]').click();
+        cy.get('[data-testid="ShowHeader"]').contains('Credit Karma');
+        cy.get('[data-testid="ShowBackLink"]').click();
 
-      cy.get('[data-testid="WorkSection--about"]').contains('About Me');
-
-      cy.get('[data-testid="WorkSection--career"]').contains('Career');
-      cy.get('[data-testid="WorkItem--/work/credit-karma"]').click();
-      cy.get('[data-testid="ShowHeader"]').contains('Credit Karma');
-      cy.get('[data-testid="ShowBackLink"]').click();
-
-      cy.get('[data-testid="WorkSection--projects"]').contains('Projects');
-      cy.get('[data-testid="WorkItem--/projects/portfolio"]').click();
-      cy.get('[data-testid="ShowBackLink"]').click();
-    });
-  });
-
-  describe('start', () => {
-    beforeEach(() => {
-      cy.visit('/start');
+        cy.get('[data-testid="WorkSection--projects"]').contains('Projects');
+        cy.get('[data-testid="WorkItem--/projects/portfolio"]').click();
+        cy.get('[data-testid="ShowBackLink"]').click();
+      });
     });
 
-    it('visits start', () => {
-      cy.matchImageSnapshot({ name: 'start' });
+    describe('start', () => {
+      beforeEach(() => {
+        cy.visit('/start');
+      });
+
+      it('visits start', () => {
+        cy.matchImageSnapshot({ name: 'start' });
+      });
     });
   });
 });
